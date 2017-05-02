22296875c78d0827934f984f573e9adcdea543cb
Snahider <snahider@gmail.com>
2017-05-02 04:58:45 -0500

    firt commit

---
+++ demo/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var os = require('os');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200);
+  res.end('Hello Kubernetes! 1.0 - ' +os.hostname());
+});
+
+server.listen(8080);
+
+console.log("Server running at http://127.0.0.1:8080/");
